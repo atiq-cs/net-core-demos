826c924696dc57af570a64232f418168f6a2ade9
Atiq Rahman <atiqcs@outlook.com>
2018-07-05 22:08:24 -0700

    .net core first console hello added

---
+++ P1_Console-Hello-World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01_Console {
+  class Program {
+    static void Main(string[] args) {
+      var cityList = new List<string> { "San Jose", "Santa Clara", "Redwood" };
+      cityList.Sort();
+      foreach (var city in cityList)
+        Console.WriteLine(city);
+    }
+  }
+}
