619d34a6abbd6fada4a457f329b3e8289502e823
Atiq Rahman <atiqcs@outlook.com>
2018-07-06 13:00:51 -0700

    mvc EF DB hello application added

---
+++ P2_MVC_SQLDB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace P2_MVC_SQLDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
